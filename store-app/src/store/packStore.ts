cd910429d8a32e76b9d7920949804e59fd764511
Hanna Muriel <hannamuriel2006@gmail.com>
2026-06-03 10:39:44 -0500

    fix: estabilizar referencias de funciones en usePackStore con useCallback

--- store-app/src/store/packStore.ts
+++ store-app/src/store/packStore.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, AppDispatch } from './index';
 import { fetchPacksThunk, deletePackThunk, updatePackThunk, uploadImageThunk, clearErrorAction } from './packSlice';
@@ -8,25 +9,25 @@ export const usePackStore = () => {
   const dispatch = useDispatch<AppDispatch>();
   const packState = useSelector((state: RootState) => state.pack);
 
-  const fetchPacks = async () => {
+  const fetchPacks = useCallback(async () => {
     await dispatch(fetchPacksThunk()).unwrap();
-  };
+  }, [dispatch]);
 
-  const deletePack = async (id: string) => {
+  const deletePack = useCallback(async (id: string) => {
     await dispatch(deletePackThunk(id)).unwrap();
-  };
+  }, [dispatch]);
 
-  const updatePack = async (id: string, data: Partial<Pack>) => {
+  const updatePack = useCallback(async (id: string, data: Partial<Pack>) => {
     return await dispatch(updatePackThunk({ id, data })).unwrap();
-  };
+  }, [dispatch]);
 
-  const uploadImage = async (id: string, file: File) => {
+  const uploadImage = useCallback(async (id: string, file: File) => {
     await dispatch(uploadImageThunk({ id, file })).unwrap();
-  };
+  }, [dispatch]);
 
-  const clearError = () => {
+  const clearError = useCallback(() => {
     dispatch(clearErrorAction());
-  };
+  }, [dispatch]);
 
   return {
     ...packState,
